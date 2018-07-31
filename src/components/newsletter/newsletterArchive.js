d9f8058d6ed7b08e12c5e62b96ec02f85853f742
Talmage <utilitytitan@gmail.com>
2018-07-31 04:33:45 +0000

    added new things

--- src/components/newsletter/newsletterArchive.js
+++ src/components/newsletter/newsletterArchive.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 
 function ArchiveItem({title, date}) {
     return (
-        <div className='archive-item'>
-        <div className='archive-item__title'>{title}</div>
-        <div className= 'archive-item__items'>{date}</div>
+        <div className='archive-item archive-items__item'>  
+            <div className='archive-item__title'>{title}</div>
+            <div className='archive-item__date'>
+                { date.getMonth()+1 }
+                /
+                { date.getDate() }
+                /
+                { date.getFullYear()-2000 }
+            </div>
         </div>
-        )
+    )
 }
 class  NewsletterArchive extends Component {
     render(){
@@ -14,12 +20,11 @@ class  NewsletterArchive extends Component {
         <div className='newsletter-archive'>
         <div className='newsletter-archive__title'>Archive</div>
         <div className='newsletter-archive__items archive-items'> 
-    <ArchiveItem title='hey' date='this is the date' />
-     <ArchiveItem title='hey' date='this is the date' />
-      <ArchiveItem title='hey' date='this is the date' />
-       <ArchiveItem title='hey' date='this is the date' />
-        <ArchiveItem title='hey' date='this is the date' />
-         <ArchiveItem title='hey' date='this is the date' />
+    <ArchiveItem title='hey' date={new Date()}/>
+    <ArchiveItem title='hey' date={new Date()}/>
+    <ArchiveItem title='hey' date={new Date()}/>
+    <ArchiveItem title='hey' date={new Date()}/>
+     
         </div>
         </div>
         )
