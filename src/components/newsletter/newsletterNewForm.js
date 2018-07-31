ceb577422c5d75084b8da5ca56ef56ce24bf7058
Talmage <utilitytitan@gmail.com>
2018-07-31 17:01:09 +0000

    added some styles

---
+++ src/components/newsletter/newsletterNewForm.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+
+import { FormTitle } from '../formTitle';
+import { FormInput, FormButton } from '../formFields';
+import TextLink from '../textLink';
+
+class NewNewsletterForm extends Component {
+  render() {
+ const { handleSubmit } = this.props;
+      
+    return (
+      <form onSubmit={handleSubmit} className='new-newsletter-form'>
+        <FormTitle className='new-newsletter-form__title' text='Login'/>
+        <Field className='new-newsletter-form__email' placeholder='Enter Email' component={FormInput} name='email' type='email' title="Email"/>
+        
+      </form>
+    );
+  }
+}
+
+NewNewsletterForm = reduxForm({
+  form: 'newsletter'
+})(NewNewsletterForm)
+
+export default NewNewsletterForm;
