a87796c131359d4019b18bceef663f3d15a830d9
Talmage <utilitytitan@gmail.com>
2018-08-06 18:00:40 +0000

    added new features

--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import Icon from '../icon';
 import Button from '../button';
+
+import AnimateHeight from 'react-animate-height';
 class RequestsItem extends Component{
-   
+   constructor(){
+       super()
+       this.state = {
+           height: 0
+       }
+   }
+    
+    toggleDropdown = () => {
+        if(this.state.height==0){
+            this.setState({height: 'auto'})
+        }else{
+            this.setState({height: 0})
+        }
+    }
     
     render(){
         return (
@@ -10,30 +25,41 @@ class RequestsItem extends Component{
      
             <Icon className='requests-item__icon' icon='fas fa-exclamation-triangle'/>
             <div className='requests-item__title'>
-            Yo my door ded
+            <div className='requests-item__title__text'>Yo my door ded</div>
+              <Icon callback={() => this.toggleDropdown()} className='requests-item__title__arrow' icon='fas fa-sort-down'/>
             </div>
             <div className='requests-item__tenant-unit'>
             Max - Unit 420
             </div>
             
-            <Icon className='requests-item__arrow' icon='fas fa-sort-down'/>
+          
            
             <div className='requests-item-date'>
             09/15/2018
             </div>
             <Button className='requests-item__move' icon='fas fa-wrench' callback={console.log('tryna move')}/>
+            
+           
              <div className='requests-item__description'>
+              <AnimateHeight
+            duration={300}
+            height={this.state.height}
+            >
+               <div className='item-description'>
                 <img
-                className='requests-item__description-img'
+                className='item-description__img'
                 src='http://via.placeholder.com/160x94'
                 />
-                <p className='requests-item__description-text'>
+                <p className='item-description__text'>
                 Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
         Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
         Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
         Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
                 </p>
                 </div>
+                   </AnimateHeight>
+                </div>
+             
             </div>
            
             
